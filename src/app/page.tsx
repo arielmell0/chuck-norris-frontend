907eaa52acb658f002cbdc0a606dd28ec9512667
arielmell0 <arielmello@rede.ulbra.br>
2024-06-24 02:04:18 -0300

    feat: fun fact generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
+"use client"
+
 import ApolloClientProvider from "@/components/ApolloClientProvider";
 import { Button } from "@/components/ui/button";
 import client from "@/lib/apolloClient";
 import { gql } from "@apollo/client";
+import { useState } from "react";
 
 const GET_HERO = gql(`{ getRandomFact }`)
 
-const Home = async () => {
-  const { data } = await client.query({
-    query: GET_HERO
-  });
-  console.log("🚀 ~ Home ~ data:", data)
+const Home = () => {
+  const [funFact, setFunFact] = useState('')
+  const onFunFactClick = async () => {
+    const { data } = await client.query({
+      query: GET_HERO,
+      fetchPolicy: 'no-cache'
+    });
+    console.log("🚀 ~ onFunFactClick ~ data:", data.getRandomFact)
+    setFunFact(data.getRandomFact)
+  }
   return (
     <>
       <div className="flex flex-row justify-center items-center w-screen h-screen">
-        <Button>
-          Fun fact!
-        </Button>
+        <div className="flex flex-col justify-center items-center gap-6">
+          <>{funFact}</>
+          <Button onClick={onFunFactClick}>
+            Fun fact!
+          </Button>
+        </div>
       </div>
     </>
   );
