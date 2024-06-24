772f709060f116d47544c7b3a9dd68d0cd229c6d
arielmell0 <arielmello@rede.ulbra.br>
2024-06-24 00:22:12 -0300

    feat: button and container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Hello world</>
+    <>
+      <div className="flex flex-row justify-center items-center w-screen h-screen">
+        <Button>
+          Fun fact!
+        </Button>
+      </div>
+    </>
   );
 }
