27ff8a5b20b87cc17526042e44405fe6ae023da9
arielmell0 <arielmello@rede.ulbra.br>
2024-06-24 22:04:54 -0300

    feat: chuck norris image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import { Button } from "@/components/ui/button";
 import { ButtonLoading } from "@/components/ui/button-loading";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import useRandomFact from "./hooks/useRandomFact";
-
+import Image from "next/image";
+import ChuckNorris from '/public/chuck-norris.png';
 const Home = () => {
   const { fetchRandomFact, funFact, loadingFunFact, categories, handleSelectedCategory } = useRandomFact()
 
   return (
     <>
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <div className="flex flex-col justify-center items-center w-screen h-screen">
-        <div className="flex flex-col justify-center items-center gap-4">
+      <div className="flex flex-col justify-start items-center w-screen h-screen">
+        <div className="px-20 mt-40 hover:animate-pulse">
+          <Image
+            width={400}
+            src={ChuckNorris}
+            alt="chuck norris image"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4 mt-20">
           <div className="px-10 mb-4 text-center">{funFact}</div>
           <Select onValueChange={handleSelectedCategory}>
             <SelectTrigger className="w-[180px]">
